09f2b0fb1ec02444ad8ae2600488b3a8ce74b538
David Bing <davidb.devops@gmail.com>
2020-10-11 15:07:09 -0500

    update field.js: add forEach

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -3,14 +3,17 @@ const plantedField = [];
 export const addPlant = (seedObj) => {
     
     if (Array.isArray(seedObj) === true) {
-        for (let i = 0; i < seedObj.length; i++) {
-            plantedField.push(i)
+        seedObj.forEach(element => {
+            plantedField.push(element)
+        });
+          
         }
+        return addPlant;
     }
-    plantedField.push(seedObj);
 
-    return addPlant;
-}
+    
+
+
 
 export const usePlants = () => {
     return plantedField;
