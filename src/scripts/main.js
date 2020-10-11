7eb11d43a301aa7011335df507f5297d220b3fc4
David Bing <davidb.devops@gmail.com>
2020-10-11 09:41:53 -0500

    update main.js: temp. include import statements and create*() calls to support debugging

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ import { createSoybean } from './seeds/soybean.js'
 import { createCorn } from './seeds/corn.js'
 import { createSunflower } from './seeds/sunflower.js'
 import { createAsparagus } from './seeds/asparagus.js'
+import { createWheat } from './seeds/wheat.js'
+import { createPotato } from './seeds/potato.js'
 
 
 console.log("Welcome to the main module")
@@ -12,8 +14,10 @@ const soybeanSeed = createSoybean();
 const cornSeed = createCorn();
 const sunflowerSeed = createSunflower();
 const asparagusSeed = createAsparagus();
+const wheatSeed = createWheat();
+const potatoSeed = createPotato();
 
-console.log(soybeanSeed, cornSeed, sunflowerSeed, asparagusSeed)
+console.log(soybeanSeed, cornSeed, sunflowerSeed, asparagusSeed, wheatSeed, potatoSeed )
 
 
 
