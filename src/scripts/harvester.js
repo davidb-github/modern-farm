912b7ba3225b4dfd303852378dda818131bab1cf
David Bing <davidb.devops@gmail.com>
2020-10-14 15:15:39 -0500

    implement harvestPlants component

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,17 +1,36 @@
 const seedObjectArray = [];
 
 export const harvestPlants = (plantedField) => {
+  debugger
+  if (Array.isArray(plantedField)) {
+    console.log('array.isarray returned TRUE');
+    plantedField.forEach(element => {
+      //pull out value for output key
+      //insert that many of the object into the seedObjectArray
 
-    if (Array.isArray(plantedField) ) {
-        plantedField.forEach(element => {
-            seedObjectArray.push(element.output)
-        });  
-      } else {
-        seedObjectArray.push(plantedField);
+      if (element.plant !== 'corn') {
+        let outputVal = 0;
+        outputVal = element.output;
+        for (let i = 0; i < outputVal; i++) {
+          seedObjectArray.push(element);
+        }
       }
-      return seedObjectArray.slice();
-    }
-    
+      // except for corn, only insert half output value.
+      else {
+        let cornIndex = element.output / 2;
+        // do math on property output and then use for loop
+        for (let i = 0; i < cornIndex; i++) {
+          seedObjectArray.push(element);;
+        }
+      }
+    });
+  } else {
+    console.log('array.isarray returned FALSE, check the code');
+
+  }
+  return seedObjectArray.slice();
+}
+
 
 /*
 Create a scripts/harvester.js module.
@@ -26,8 +45,4 @@ For example, if the current plant is a peanut object with an output of 2...
 
 Again, the exception is corn. Half of your corn will be sold to cattle ranchers,
 so only half of the output of each corn plant will be added to the array that this function returns.
-
-
-
-
 */
